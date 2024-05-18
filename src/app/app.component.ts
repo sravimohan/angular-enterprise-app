c7d50bfce33b4580bb39b72c7c93e6c568de185f
Ravi Mohan <ravi.mohan@roonga.com.au>
2024-05-19 09:18:06 +1000

    init : setup with tailwind and material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  RouterLinkWithHref,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    RouterLinkWithHref,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-enterprise-app';
